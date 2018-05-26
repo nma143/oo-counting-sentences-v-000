c08777d08e0442d01d4c473d1869a5afe310f247
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-26 20:45:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    @parts = self.split(/[.!?](\s|\z)/)
+    @count = 0
+    @parts.each do |part|
+      if part != " "
+        @count = @count + 1
+      end
+    end
+    #binding.pry
+    @count
   end
 end
